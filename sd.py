a9010ab9d6ce5132e73d59b16b81b2fa24ec34ec
hamidreza-bahrami <150763463+hamidreza-bahrami@users.noreply.github.com>
2024-11-17 09:02:08 -0800

    Update sd.py

--- sd.py
+++ sd.py
@@ -33,6 +33,8 @@ email_password = "htth eind kniy bgst"
 
 context = ssl.create_default_context()
 
+st.set_page_config(page_title='پیشگیری از خودکشی - RoboAi', layout='centered', page_icon='🩺')
+
 vector = pickle.load(open('vector.pkl', 'rb'))
 load_model = pickle.load(open('model.pkl', 'rb'))
 translator = Translator()
@@ -54,10 +56,18 @@ def thought(text):
   return prediction
 
 def show_page():
-    st.write("<h1 style='text-align: center; color: blue;'>مدل تشخیص تمایل به خودکشی</h1>", unsafe_allow_html=True)
-    st.write("<h2 style='text-align: center; color: gray;'>تشخیص بر اساس تحلیل متن کاربر</h2>", unsafe_allow_html=True)
-    st.write("<h4 style='text-align: center; color: gray;'>طراح: حمیدرضا بهرامی</h4>", unsafe_allow_html=True)
-    st.link_button("Robo-Ai بازگشت به", "https://robo-ai.ir")
+    st.write("<h3 style='text-align: center; color: gold;'>سامانه تشخیص و پیشگیری از خودکشی 🩺</h3>", unsafe_allow_html=True)
+    st.write("<h6 style='text-align: center; color: white;'>RoboAi طراحی شده توسط</h6>", unsafe_allow_html=True)
+    st.link_button("Robo-Ai.ir بازگشت به", "https://robo-ai.ir")
+    with st.sidebar:
+        st.write("<h4 style='text-align: center; color: white;'>تشخیص و پیشگیری از خودکشی</h4>", unsafe_allow_html=True)
+        st.write("<h4 style='text-align: center; color: white;'>با تحلیل متن کاربر</h4>", unsafe_allow_html=True)
+        st.divider()
+        st.write("<h6 style='text-align: center; color: white;'>طراحی و توسعه</h6>", unsafe_allow_html=True)
+        st.write("<h6 style='text-align: center; color: white;'>حمیدرضا بهرامی</h6>", unsafe_allow_html=True)
+
+    container = st.container(border=True)
+    container.write("<h6 style='text-align: right; color: white;'>تشخیص احتمال بروز خودکشی از متن 💬</h6>", unsafe_allow_html=True)
 
     number = st.text_input('لطفا شماره تماس خود را وارد کنید')
     text = st.text_area('افکار خود را با من درمیان بگذارید',height=None,max_chars=None,key=None)
@@ -65,22 +75,36 @@ def show_page():
     if st.button('تحلیل افکار'):
         if text == "":
             with st.chat_message("assistant"):
-                with st.spinner('''درحال بررسی لطفا صبور باشید'''):
-                    time.sleep(3)
+                with st.spinner('''درحال تحلیل'''):
+                    time.sleep(2)
                     st.success(u'\u2713''تحلیل انجام شد')
-                    st.write("<h4 style='text-align: right; color: gray;'>لطفا افکار خود را بنویسید تا بتوانم تحلیل کنم</h4>", unsafe_allow_html=True)
+                    text1 = 'لطفا متن را وارد کنید'
+                    def stream_data1():
+                        for word in text1.split(" "):
+                            yield word + " "
+                            time.sleep(0.09)
+                    st.write_stream(stream_data1)
         
         else:
             out = translator.translate(text)
             prediction_class = thought(out.text)
             if prediction_class == [1]:
                 with st.chat_message("assistant"):
-                    with st.spinner('''درحال بررسی لطفا صبور باشید'''):
-                        time.sleep(3)
+                    with st.spinner('''درحال تحلیل ، لطفا صبور باشید'''):
+                        time.sleep(2)
                         st.success(u'\u2713''تحلیل انجام شد')
-                        st.write("<h4 style='text-align: right; color: gray;'>.بر اساس تحلیل من، شما به خودکشی تمایل دارید</h4>", unsafe_allow_html=True)
-                        st.write("<h4 style='text-align: right; color: gray;'>.برای تسکین فوری افکار خود با فردی از نزدیکان صحبت کنید</h4>", unsafe_allow_html=True)
-                        st.write("<h4 style='text-align: right; color: gray;'>.و برای درمان قطعی به روانشناس مراجعه کنید</h4>", unsafe_allow_html=True)
+                        text1 = 'بر اساس تحلیل من ، علائمی از خطر خودکشی در متن موردنظر دیده می شود'
+                        text2 = 'متن شما به همراه اطلاعات تکمیلی به سرور اصلی ارسال گردید'
+                        def stream_data1():
+                            for word in text1.split(" "):
+                                yield word + " "
+                                time.sleep(0.09)
+                        st.write_stream(stream_data1)
+                        def stream_data2():
+                            for word in text2.split(" "):
+                                yield word + " "
+                                time.sleep(0.09)
+                        st.write_stream(stream_data2)
                         subject = number
                         body = ("کاربر در حال خودکشی شناسایی شد")
                         em = EmailMessage()
@@ -94,11 +118,16 @@ def show_page():
                         
             else:
                 with st.chat_message("assistant"):
-                    with st.spinner('''درحال بررسی لطفا صبور باشید'''):
-                        time.sleep(3)
+                    with st.spinner('''درحال تحلیل ، لطفا صبور باشید'''):
+                        time.sleep(2)
                         st.success(u'\u2713''تحلیل انجام شد')
-                        st.write("<h4 style='text-align: right; color: gray;'>.بر اساس تحلیل من، بیشتر افکار شما سالم است</h4>", unsafe_allow_html=True)
-                        st.write("<h4 style='text-align: right; color: gray;'>.در صورت وجود افکار متمایل به خودکشی، به روانشناس مراجعه کنید</h4>", unsafe_allow_html=True)
+                        st.success(u'\u2713''تحلیل انجام شد')
+                        text3 = 'بر اساس تحلیل من ، علائمی از خطر خودکشی در متن موردنظر دیده نمی شود'
+                        def stream_data3():
+                            for word in text3.split(" "):
+                                yield word + " "
+                                time.sleep(0.09)
+                        st.write_stream(stream_data3)
     else:
         pass
 
@@ -106,5 +135,3 @@ def show_page():
         st.cache_data.clear()
         st.rerun()
 show_page()
-
-
